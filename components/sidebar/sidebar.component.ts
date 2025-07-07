20bec80e99b0c7aa9c05daab639bf8b5bc633d09
camilopinheiro-clina <camilopinheiro@live.com>
2025-07-07 13:13:59 -0300

    Update sidebar.component.ts

--- components/sidebar/sidebar.component.ts
+++ components/sidebar/sidebar.component.ts
@@ -116,7 +116,7 @@ export class SidebarComponent implements OnInit, OnDestroy {
         title: "Assinaturas",
         icon: "icon-calendar-check-2",
         menuUrl: "/subscription/management",
-        isActive: true,
+        isActive: false,
         mode: ItemModeEnum.PS,
         system: SystemEnum.DASHBOARD,
         url:""
