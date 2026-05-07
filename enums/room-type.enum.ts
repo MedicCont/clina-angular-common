e2d80d66d60e8b7fd2d8c285d38c412634fd8307
Daniel Oliveira <dan.araujjo@gmail.com>
2026-05-07 11:46:37 -0300

    #11002489741 - feat: adiciona tipo Terapia Ocupacional e ajustes visuais nos cards

--- enums/room-type.enum.ts
+++ enums/room-type.enum.ts
@@ -4,5 +4,16 @@ export enum RoomTypeEnum {
   DENTAL = 'DENTAL',
   PHYSICAL_EXAM = 'PHYSICAL_EXAM',
   FIT = 'FIT',
+  OCCUPATIONAL_THERAPY = 'OCCUPATIONAL_THERAPY',
   OTHERS = 'OTHERS',
+}
+
+export enum RoomTypeIcon {
+  DIVAN = 'far fa-couch',
+  TRADITIONAL = 'far fa-user-md',
+  DENTAL = 'far fa-tooth',
+  PHYSICAL_EXAM = 'far fa-stethoscope',
+  FIT = 'far fa-dumbbell',
+  OCCUPATIONAL_THERAPY = 'far fa-hand-holding-heart',
+  OTHERS = 'far fa-hospital',
 }
